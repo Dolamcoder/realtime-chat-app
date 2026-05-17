147767aba6f98a0aa02cd344f1b27e22d3126cd2
DO LAM <thinhphu479@gmail.com>
2026-05-18 00:32:09 +0700

    design interface signin, signup by shadcn and validate data by zod

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignUpPage from "./pages/SignUpPage";
+import SighInPage from "./pages/SignInPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
+
 function App() {
   return (
     <>
-      hello world
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signin" element={<SighInPage />} />
+          <Route path="/" element={<ChatAppPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/ChatAppPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+const ChatAppPage=()=>{
+    return (
+        <>ChatApp page</>
+    )
+}
+export default ChatAppPage;
